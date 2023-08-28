3c8e86d1b8b1d28eb4b5eabe629a9d34612c2fa3
ff137 <ff137@proton.me>
2023-08-28 15:15:37 +0200

    :art: formatting

--- scripts/create-slack-app.sh
+++ scripts/create-slack-app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xeuv
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-manifest="$( cat ${SCRIPT_DIR}/../resources/slack-manifest.json  | envsubst )"
+manifest="$(cat ${SCRIPT_DIR}/../resources/slack-manifest.json | envsubst)"
 
 function createApp() {
   curl -s -XPOST https://slack.com/api/apps.manifest.create \
