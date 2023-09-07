2d9845e5c709be0188fc97ae465175dd8d5332c3
ff137 <ff137@proton.me>
2023-09-07 16:55:23 +0200

    :sparkles: add conversationId and context management

--- lambda/src/main/scala/slackBotLambda/AiHandler.scala
+++ lambda/src/main/scala/slackBotLambda/AiHandler.scala
@@ -3,24 +3,52 @@ package slackBotLambda
 import com.xebia.functional.xef.prompt.JvmPromptBuilder
 import com.xebia.functional.xef.prompt.Prompt
 import com.xebia.functional.xef.scala.conversation.*
+import com.xebia.functional.xef.store.ConversationId
+import scala.collection.mutable
+import com.xebia.functional.xef.prompt.PromptBuilder
 
+case class OnboardingResult(
+    fullName: Option[String] = None,
+    email: Option[String] = None,
+    cellphone: Option[String] = None
+)
 object AiHandler {
+  private def createBuilder(): PromptBuilder = {
+    new JvmPromptBuilder()
+      .addSystemMessage(
+        "You are an onboarding assistant. " +
+          "If you receive a first message from a user, your job is ask for " +
+          "the following info: " +
+          "Name; Email; Cellphone. " +
+          "Be friendly." +
+          "If they only give one attribute at a time, that's fine, just remind " +
+          "them until you have all 3 fields." +
+          "If a user says no, they don't want to give their email or cellphone, then that is fine, but we at least need a name"
+      )
+  }
 
-  def getAiResponse(input: String): String = {
-    conversation {
-      val builder = new JvmPromptBuilder()
-        .addSystemMessage(
-          "You are an onboarding assistant. " +
-            "Your assignment is to obtain the following info from the user: " +
-            "Full Name; Email; Cellphone. " +
-            "Be courteous."
-        )
-        .addUserMessage(
-          input
-        )
+  // Define a map from conversationId to JvmPromptBuilder
+  private val builders: mutable.Map[String, PromptBuilder] = mutable.Map()
 
-      val response = promptMessage(builder.build())
-      response
-    }
+  def getAiResponse(input: String, conversationId: String): String = {
+    scribe.info(s"Get AI response for message: $input, for conversationId: $conversationId")
+    // Get the builder for this conversationId, or create a new one if it doesn't exist
+    val builder = builders.getOrElseUpdate(
+      conversationId,
+      createBuilder()
+    )
+
+    // Add the user message to the builder
+    builder.addUserMessage(input)
+
+    // Create a new conversation with the specific conversationId
+    conversation(
+      {
+        // Get the AI's response
+        val response = promptMessage(builder.build())
+        response
+      },
+      conversationId = Some(ConversationId(conversationId))
+    )
   }
 }
