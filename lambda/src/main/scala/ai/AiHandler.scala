b9cc552a46d13a35a37c4e4721cda6332cb5d9df
ff137 <ff137@proton.me>
2023-09-11 14:14:31 +0300

    :truck: move OnboardingResult to own module

--- lambda/src/main/scala/ai/AiHandler.scala
+++ lambda/src/main/scala/ai/AiHandler.scala
@@ -6,15 +6,7 @@ import com.xebia.functional.xef.scala.conversation.*
 import com.xebia.functional.xef.store.ConversationId
 import scala.collection.mutable
 import com.xebia.functional.xef.prompt.PromptBuilder
-import io.circe.Decoder
-
-case class OnboardingResult(
-    @Description("The next message that you want to send to the user") nextMessageToUser: String,
-    @Description("The full name as obtained from the user") fullName: Option[String] = None,
-    @Description("The email address as obtained from the user") email: Option[String] = None,
-    @Description("The cellphone number as obtained from the user") cellphone: Option[String] = None
-) derives SerialDescriptor,
-      Decoder
+import ai.model.OnboardingResult
 
 object AiHandler {
   private def createBuilder(): PromptBuilder = {
