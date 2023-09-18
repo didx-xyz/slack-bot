19e89bf974a95fb523e90ede7544d110d6e1ba18
ff137 <ff137@proton.me>
2023-09-18 16:43:43 +0200

    :construction: add handlings for QueryingOpportunity state

--- lambda/src/main/scala/ai/AiHandler.scala
+++ lambda/src/main/scala/ai/AiHandler.scala
@@ -2,7 +2,9 @@ package ai
 
 import ai.model.ChatState
 import ai.handler.OnboardingHandler
-
+import dev.langchain4j.data.segment.TextSegment
+import ai.embedding.EmbeddingHandler
+import dev.langchain4j.store.embedding.EmbeddingMatch
 object AiHandler {
 
   def getAiResponse(
@@ -13,10 +15,17 @@ object AiHandler {
     scribe.info(
       s"Get AI response for message: $input, for conversationId: $conversationId, in state: $state"
     )
+
     state match
       case ChatState.Onboarding            => OnboardingHandler.getResponse(input, conversationId)
-      case ChatState.QueryingOpportunities => ???
+      case ChatState.QueryingOpportunities => {
+        val trimInput                                   = input.split("query:").last
+        val embeddingMatch: EmbeddingMatch[TextSegment] =
+          EmbeddingHandler.findMostRelevantFromQuery(trimInput)
+        val response                                    =
+          s"Got embedding match with: ${embeddingMatch.embedded().text()}, with score: ${embeddingMatch.score()}"
+        response
+      }
       case ChatState.Done                  => ???
-
   }
 }
